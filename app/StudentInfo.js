6d78d44d0afd297e42adc18f7d1b115f6056cd97
Garry Jr Dayag <garrydayagjr@gmail.com>
2023-09-14 14:22:23 -0600

    Added week 2

---
+++ app/StudentInfo.js
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+
+export default function StudentInfo() {
+    return (
+        <>
+            <div>
+                <p>Name: Garry Jr Dayag</p>
+                <p>Course: CPRG 306 F</p>
+                <Link href="https://github.com/GarryLoopy">https://github.com/GarryLoopy</Link>
+            </div>
+        </>
+    );
+}
