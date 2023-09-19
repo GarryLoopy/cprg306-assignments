ba7da078185ba284d279d3e3fd85176bd32b8923
Garry Jr Dayag <garrydayagjr@gmail.com>
2023-09-19 13:56:11 -0600

    Update item.js

--- app/week3/item.js
+++ app/week3/item.js
@@ -1,6 +1,6 @@
 export default function Item({ name, quantity, category}) {
     return (
-        <div class="p-2 m-4 bg-violet-800 hover:bg-violet-900 max-w-sm rounded-lg">
+        <div class="p-2 m-4 bg-violet-800 hover:bg-violet-900 max-w-sm rounded-r-xl">
             <h2 class="text-xl font-bold">{name}</h2>
             <p class="text-sm">Buy {quantity} in {category}</p>
         </div>
