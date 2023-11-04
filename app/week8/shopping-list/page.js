3937e96e55ddeb15aa6e2ae5aa399a1a009124c3
Garry Jr Dayag <garrydayagjr@gmail.com>
2023-11-04 17:55:32 -0600

    Update page.js

--- app/week8/shopping-list/page.js
+++ app/week8/shopping-list/page.js
@@ -67,7 +67,7 @@ export default function Page() {
                 </div>
                 :
                 <div className="text-center">
-                    <h1 className="text-2xl mb-4">Sorry but we hate guests. You need to be signed in first to view this page</h1>
+                    <h1 className="text-2xl mb-4">Sorry, but we hate guests. You need to be signed in first to view this page</h1>
                     <Link href="/week8" className="hover:text-white active:bg-slate-700 hover:bg-gray-800 hover:border-gray-700 text-lg py-2 px-4 border border-gray-800 bg-gray-950 rounded-md">Go back to week 8</Link>
                 </div>
             }
