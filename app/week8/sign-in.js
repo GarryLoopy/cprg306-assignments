8886483cf1f0078527c6a7fe3d4a59ef099e4fbe
Garry Jr Dayag <garrydayagjr@gmail.com>
2023-11-04 18:24:37 -0600

    Update sign-in.js

--- app/week8/sign-in.js
+++ app/week8/sign-in.js
@@ -49,6 +49,7 @@ export default function SignIn() {
                 user && (
                     <div className="flex justify-between">
                         <div className="flex flex-row gap-4 border-gray-800 border p-4 rounded-xl bg-gray-950 mt-auto mb-auto">
+                            
                             <img src={user.photoURL} className="w-10 h-10 rounded-full"/>
                             <div>
                                 <p className="text-md">{user.displayName}</p>
