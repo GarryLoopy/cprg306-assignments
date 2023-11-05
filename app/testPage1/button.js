355ec1e452f5732680fdf0042ecf5c29fc584c85
Garry Jr Dayag <garrydayagjr@gmail.com>
2023-11-04 18:21:09 -0600

    Deleted alot of stuff

--- app/testPage1/button.js
+++
@@ -1,5 +0,0 @@
-export default function Button({ content }) {
-    return (
-        <button class="px-2 py-3 text-center bg-slate-900 hover:bg-slate-800 active:bg-slate-700 rounded-md">{content}</button>
-    );
-}
